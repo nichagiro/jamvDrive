928a0da1ab1ff92cc478fe1939a220b6234b340a
Nicolas Chamorro <nichagiro@gmail.com>
2025-06-16 15:44:27 -0500

    add firebase hosting

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
   images: {
     unoptimized: true,
   },
+  output: "export"
 }
 
 export default nextConfig
