ee5af720e9b8e9768806439f940c70f08e1c42a3
Nicolas Chamorro <nichagiro@gmail.com>
2026-01-31 17:24:26 -0500

    update url ext

--- src/components/Layout/Header/Navigation/menuData.tsx
+++ src/components/Layout/Header/Navigation/menuData.tsx
@@ -7,5 +7,5 @@ export const headerData: HeaderItem[] = [
   { label: "Asesoria", href: "/#asesoria" },
   { label: "Porque elegirnos", href: "/#porque-elegirnos" },
   { label: "Contacto", href: "/contacto" },
-  { label: "Inventario", href: "https://inventariojamvdrive.rf.gd/inventario", type: "button", noMobile: true },
+  { label: "Inventario", href: "https://inventario.jamvexpress.com.co/", type: "button", noMobile: true },
 ];
